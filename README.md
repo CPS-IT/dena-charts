82fabb9da4d2d9f093befdddfe104b9ee786fa4b
Dirk Wenzel <wenzel@cps-it.de>
2017-08-30 16:58:26 +0200

    [TASK] extension configuration files added.

---
+++ README.md
@@ -0,0 +1,6 @@
+DENA Charts
+===========
+
+Charts allows two display data sets as charts. This is a TYPO3 extension for the DENA project.
+
+This extension has been developed at [CPS IT GmbH](https://cps-it.de).
---
+++ ext_emconf.php
@@ -0,0 +1,39 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "dena_charts
+ *
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+  'title' => 'Charts',
+  'description' => 'Charts allows two display data sets as charts. This is a TYPO3 extension for the DENA project.',
+  'author' => 'Dirk Wenzel',
+  'author_email' => 'dirk.wenzel@cps-it.de',
+  'author_company' => '',
+  'state' => 'beta',
+  'uploadfolder' => '0',
+  'createDirs' => '',
+  'clearCacheOnLoad' => 1,
+  'version' => '0.0.0',
+  'constraints' =>
+  array (
+    'depends' =>
+    array (
+      'typo3' => '6.2.0-8.99.99',
+      'php' => '5.6.0-0.0.0',
+    ),
+    'conflicts' =>
+    array (
+    ),
+    'suggests' =>
+    array (
+    ),
+  ),
+  '_md5_values_when_last_written' => 'foo',
+);
+
