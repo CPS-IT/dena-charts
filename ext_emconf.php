8e42489c82cadd998d1fec036e6bff1ae2c889dd
Arend Maubach <a.maubach@familie-redlich.de>
2022-12-20 15:32:59 +0100

    set ver 0.1.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -19,7 +19,7 @@ $EM_CONF[$_EXTKEY] = array(
     'uploadfolder' => '0',
     'createDirs' => '',
     'clearCacheOnLoad' => 1,
-    'version' => '0.1.0',
+    'version' => '0.1.1',
     'constraints' =>
         array(
             'depends' =>
