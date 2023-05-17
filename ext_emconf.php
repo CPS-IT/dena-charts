c6330426c6366641290e2ddd3245e2d74ea64f46
Dirk Wenzel <wenzel@cps-it.de>
2023-05-17 14:22:11 +0200

    [RELEASE] Release of dena_charts 1.0.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -19,7 +19,7 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => false,
     'createDirs' => '',
     'clearCacheOnLoad' => 1,
-    'version' => '1.0.0',
+    'version' => '1.0.1',
     'constraints' =>
         [
             'depends' =>
